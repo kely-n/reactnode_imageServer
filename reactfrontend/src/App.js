ca988987f7a150741eb475a77acbea8d678fd1e2
kely-n <ukirukely@gmail.com>
2021-06-19 11:32:41 +0300

    empty react front end

---
+++ reactfrontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="maindiv">
+        <h3>
+          React Upload files:
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
